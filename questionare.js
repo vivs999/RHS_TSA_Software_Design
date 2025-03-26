0ba54dfc5cf9f3e5565978a418bcffa4be6da7cd
vivs999 <rochlanivivaan@gmail.com>
2025-03-26 10:23:58 -0500

    Questionare Changes Center aligned

--- questionare.js
+++ questionare.js
@@ -1,4 +1,3 @@
-
 function showPage(pageNumber) {
     document.querySelectorAll('.questionnaire-page').forEach(page => {
         page.classList.add('d-none');
@@ -12,3 +11,28 @@ function submitForm() {
 }
 
 //add code that says that if there is no value for one of the options that it will give an alert and ask to fill it out.
+
+//can someone test code below
+
+/*
+function validatePage(pageNumber) {
+    const currentPage = document.getElementById(`page-${pageNumber}`);
+    const inputs = currentPage.querySelectorAll('input, select, textarea');
+    for (let input of inputs) {
+        if (!input.value.trim()) {
+            alert('Please fill out all fields before proceeding to the next page.');
+            return false;
+        }
+    }
+    return true;
+}
+
+document.querySelectorAll('.next-button').forEach((button, index) => {
+    button.addEventListener('click', () => {
+        const currentPageNumber = index + 1;
+        if (validatePage(currentPageNumber)) {
+            showPage(currentPageNumber + 1);
+        }
+    });
+});
+*/
