3c8ef9d0610dd5c54ac7f1e654d172960f0e7810
vivs999 <rochlanivivaan@gmail.com>
2025-04-02 23:57:10 -0400

    cleanup questionare.js

--- questionare.js
+++ questionare.js
@@ -7,6 +7,10 @@ function showPage(pageNumber) {
     
 }
 
+
+let formData = {};
+
+
 function submitForm() {
     //alert('Your responses have been submitted. Thank you!');
     formData.name = document.getElementById('name').value;
@@ -21,42 +25,3 @@ function submitForm() {
     //console.log(formData);
     window.location.href = 'results.html';
 }
-
-let formData = {};
-
-// function hidePage1(){
-//     document.getElementById('page-2').classList.add('d-none');
-//     document.getElementById('page-3').classList.remove('d-none');
-// }
-
-// function hidePage2(){
-//     document.getElementById('page-3').classList.add('d-none');
-//     document.getElementById('page-4').classList.remove('d-none');
-// }
-
-
-
-//add code that says that if there is no value for one of the options that it will give an alert and ask to fill it out.
-
-//can someone test code below
-
-
-function validatePage(pageNumber) {
-    const currentPage = document.getElementById(`page-${pageNumber}`);
-    const inputs = currentPage.querySelectorAll('input, select, textarea');
-    for (let input of inputs) {
-        if (!input.value.trim()) {
-            return false;
-        }
-    }
-    return true;
-}
-
-document.querySelectorAll('.next-button').forEach((button, index) => {
-    button.addEventListener('click', () => {
-        const currentPageNumber = index + 1;
-        if (validatePage(currentPageNumber)) {
-            showPage(currentPageNumber + 1);
-        }
-    });
-});
